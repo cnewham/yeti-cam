7aad48f4c03b05bbe6167ae3c18e3f1dcad7514a
Chris Newham <c.j.newham@gmail.com>
2016-02-06 12:37:55 -0500

    added init script and logging to file

---
+++ deploy.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+scp cam.py root@yeticam:/home/yeti/cam/cam.py
+#scp -r yeti root@yeticam:/home/yeti/cam/yeti
--- yeti/common/__init__.py
+++ yeti/common/__init__.py
@@ -1,6 +1,20 @@
 __author__ = 'chris'
 import logging
+import logging.handlers
+
+LOG_LEVEL = logging.DEBUG
+LOG_FILENAME = "logs/yeticam.log"
 
 #configure logging
-logging.basicConfig(level=logging.DEBUG,
-                    format="%(asctime)-15s %(levelname)-8s %(name)-20s %(message)s")
+logging.basicConfig(level=LOG_LEVEL,
+                    format="%%(name)-12s: %(levelname)-8s %(message)s")
+
+handler = logging.handlers.RotatingFileHandler(
+              LOG_FILENAME, maxBytes=1000000, backupCount=5)
+
+formatter = logging.Formatter("%(asctime)-15s %(levelname)-8s %(name)-20s %(message)s")
+handler.setFormatter(formatter)
+
+logging.getLogger('').addHandler(handler)
+
+
